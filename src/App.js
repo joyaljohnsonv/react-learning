c03cf5a725826a0762348ef602e3e11e19a971f9
Joyal Johnson <joyaljohnsonv@gmail.com>
2023-06-06 07:40:21 +0530

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css'
+import {useState} from 'react'
 import Header from './Components/Header';
+import Counter from './Counter';
 
 
 function App() {
+  const [count,setCount] =useState(0)
   const data = 'AjaxMusic'
+  const addCount =()=>{
+    setCount(count+1)
+    console.log(count);
+  }
+  let obj = {
+    title:'1st Counter',
+    count
+  }
+  
   return (
     <div>
    
    <p>This is a Discription <br/> {data}</p>
    <Header data={data}/>
+   <button onClick={addCount}>Add</button>
+   <Counter {...obj}/>
+   <Counter title='2nd Counter' count={count}/>
    </div>
   );
 }
---
+++ src/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Counter({title,count}) {
+    
+  return (
+    <div>
+    <h2>{title} {count}</h2>  
+    </div>
+  )
+}
+
+export default Counter
